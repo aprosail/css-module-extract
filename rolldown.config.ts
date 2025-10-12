e5d57e1a29778d74af5fd95c1da504ec878aeeda
James Aprosail <aprosail@outlook.com>
2025-10-12 10:42:42 +0800

    feat: support commonjs and optimize build

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -8,12 +8,25 @@ import { dependencies } from "./package.json"
 
 const root = import.meta.dirname
 const out = join(root, "out")
+const entries: string[] = ["css", "hash"]
 
-for (const n of readdirSync(out)) rmSync(join(out, n), { recursive: true })
-export default defineConfig({
-  plugins: [dts({ sourcemap: true })],
+const lib = defineConfig({
   resolve: { alias: aliases() },
   external: [/^node:/g, ...Object.keys(dependencies), ...builtinModules],
-  input: ["css"].map((n) => join(root, `src/${n}.ts`)),
+  input: entries.map((n) => join(root, `src/${n}.ts`)),
   output: { dir: "out", format: "esm", minify: true, sourcemap: true },
 })
+
+const esModule = defineConfig({ ...lib, plugins: [dts({ sourcemap: true })] })
+const commonJS = defineConfig({
+  ...lib,
+  output: {
+    ...lib.output,
+    dir: join(out, "commonjs"),
+    format: "commonjs",
+    exports: "named",
+  },
+})
+
+for (const n of readdirSync(out)) rmSync(join(out, n), { recursive: true })
+export default defineConfig([esModule, commonJS])
